b386b84fa74c554bd9f9c13c97e3aaa289ed35b2
jmusau62-gmail <jmusau62@gmail.com>
2019-12-16 06:06:22 +0300

    added if else statement

--- src/Ceaser.java
+++ src/Ceaser.java
@@ -1,4 +1,15 @@
 public class Ceaser {
+     public static String  encrypt(String plainText,int shift){
+         if(shift>26){
+             shift=shift%26;
+         }
+         else if(shift<0){
+             shift=(shift%26)+26;
+         }
+String cipherText="";
+         int length=plainText.length();
+         return cipherText;
+     }
 
     public static  void main(String[]args){
 
