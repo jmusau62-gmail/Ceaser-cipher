25a7acc64a6e4f69bd4946bfec966a3d8f628401
jmusau62-gmail <jmusau62@gmail.com>
2019-12-16 06:39:47 +0300

    added two more if else statements to char

--- src/Ceaser.java
+++ src/Ceaser.java
@@ -1,13 +1,29 @@
 public class Ceaser {
-     public static String  encrypt(String plainText,int shift){
-         if(shift>26){
-             shift=shift%26;
+     public static String  encrypt(String plainText,int shift) {
+         if (shift > 26) {
+             shift = shift % 26;
+         } else if (shift < 0) {
+             shift = (shift % 26) + 26;
          }
-         else if(shift<0){
-             shift=(shift%26)+26;
+         String cipherText = "";
+         int length = plainText.length();
+         for (int i = 0; i < length; i++) {
+             char ch = plainText.charAt(i);
+             if (Character.isLetter(ch)) {
+                 if (Character.IsLowerCase(ch)) {
+                char c =(char)(ch+shift);
+                if(c>'z'){
+                    cipherText +=(char)(ch -(26-shift));
+                }
+                 }
+                 else if (Character.IsUpperCase(ch)) {
+                 }
+             }
+             else {
+             }
+             cipherText += ch;
          }
-String cipherText="";
-         int length=plainText.length();
+
          return cipherText;
      }
 
